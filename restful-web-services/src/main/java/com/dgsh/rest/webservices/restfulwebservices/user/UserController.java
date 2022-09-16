3fac41adf3d682cfea2ec42d999adb144dbd9e40
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-09-16 22:19:18 +0530

    10 - Response Code for POST & Enhancing POST Method to return correct HTTP Status Code and Location

--- restful-web-services/src/main/java/com/dgsh/rest/webservices/restfulwebservices/user/UserController.java
+++ restful-web-services/src/main/java/com/dgsh/rest/webservices/restfulwebservices/user/UserController.java
@@ -1,13 +1,16 @@
 package com.dgsh.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -26,8 +29,14 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public User creatUser(@RequestBody User user) {
-		return userDao.createUser(user);
+	public ResponseEntity<User> creatUser(@RequestBody User user) {
+		 userDao.createUser(user);
+		 
+		 URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				 .path("/{id}")
+				 .buildAndExpand(user.getId())
+				 .toUri();
+		 return ResponseEntity.created(location ).build();
 	}
 	
 }
