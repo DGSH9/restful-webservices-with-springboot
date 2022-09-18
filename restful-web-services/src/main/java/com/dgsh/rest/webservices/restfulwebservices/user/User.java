b7910da96dfe18781297fcee8e13987dd8e7e8da
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-09-18 19:49:07 +0530

    23 - Static Filtering for Password_____Implementing Static Filtering for REST API

--- restful-web-services/src/main/java/com/dgsh/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/dgsh/rest/webservices/restfulwebservices/user/User.java
@@ -2,7 +2,8 @@ package com.dgsh.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.Min;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -10,8 +11,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min = 3,message = "Enter Atleast 3 characters")
+	@JsonProperty("user_name")
 	private String name;
+	
 	@Past(message = "DOB must be in past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
