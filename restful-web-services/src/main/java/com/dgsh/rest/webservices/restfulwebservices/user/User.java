d41b4cd75fcc0d33976c41e5308a38d044a2c5dc
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-09-18 21:04:40 +0530

    28 - Connection h2 Database_____Creating User Entity and some test data

--- restful-web-services/src/main/java/com/dgsh/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/dgsh/rest/webservices/restfulwebservices/user/User.java
@@ -4,10 +4,16 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min = 3,message = "Enter Atleast 3 characters")
